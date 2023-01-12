b5ca2a8ce213a446759804a77527010f3e248f38
joyce sabwe <joycesabwe@gmail.com>
2023-01-12 03:05:16 +0100

    remove translation

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+
+    )
+    {
+        
+    }
+
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+
+        $url = $this->adminUrlGenerator
+        ->setController(UserCrudController::class)
+        ->setController(CategoryCrudController::class)
+        ->generateUrl();
+
+        return $this->redirect($url);
+
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -41,6 +63,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-layer-group', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-tag', Product::class);
+
+        
     }
 }
